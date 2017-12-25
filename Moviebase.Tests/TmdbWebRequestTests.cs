6a5f67bf2901930312adca376b2cc47aeae2c9bd
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2017-12-25 07:35:45 +0700

    added TmdbWebRequest tests

---
+++ Moviebase.Tests/TmdbWebRequestTests.cs
@@ -0,0 +1,51 @@
+﻿using Moviebase.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moviebase.Tests
+{
+    [TestFixture]
+    public class TmdbWebRequestTests
+    {
+        private TmdbWebRequest _tmdbWebRequest;
+
+        [SetUp]
+        public void Setup()
+        {
+            _tmdbWebRequest = new TmdbWebRequest("DDD");
+        }
+
+        [Test]
+        public void TestBuildQueryString()
+        {
+            var col = new NameValueCollection
+            {
+                {"ked", "dd" },
+                {"kesd", "ddd" },
+            };
+            var result = _tmdbWebRequest.BuildQueryString(col);
+
+            Assert.AreEqual("ked=dd&kesd=ddd", result);
+        }
+
+        [Test]
+        public void TestBuildApiUri()
+        {
+            var col = new NameValueCollection
+            {
+                {"ked", "dd" },
+                {"kesd", "ddd" },
+            };
+            var result = _tmdbWebRequest.BuildApiUri("/movie", col);
+
+            var expected = "https://api.themoviedb.org/3/movie?api_key=DDD&ked=dd&kesd=ddd";
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
