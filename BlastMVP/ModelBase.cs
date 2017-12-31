2d89f5f601c226708b09558c384070ac397d8633
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2017-12-31 09:34:27 +0700

    added abstract model

---
+++ BlastMVP/ModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace BlastMVP
+{
+    public class ModelBase : INotifyPropertyChanged
+    {
+        protected SynchronizationContext Context;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ModelBase(SynchronizationContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public void Invoke(Action invoke)
+        {
+            if (invoke == null) throw new ArgumentNullException(nameof(invoke));
+            Context.Send(_ => invoke.Invoke(), null);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            Context.Send(_ => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)), null);
+        }
+    }
+}
