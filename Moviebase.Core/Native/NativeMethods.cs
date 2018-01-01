9f85fbebdc03c4aa2f39a105e862555f0a6bf4a4
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2018-01-01 11:35:08 +0700

    using correct API signature

--- Moviebase.Core/Native/NativeMethods.cs
+++ Moviebase.Core/Native/NativeMethods.cs
@@ -9,10 +9,11 @@ namespace Moviebase.Core.Native
     [SuppressUnmanagedCodeSecurity]
     internal static class NativeMethods
     {
-        [DllImport("kernel32", CharSet = CharSet.Unicode)]
-        public static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+        [DllImport("kernel32.dll", EntryPoint = "WritePrivateProfileStringW")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool WritePrivateProfileString([In] [MarshalAs(UnmanagedType.LPWStr)] string lpAppName, [In] [MarshalAs(UnmanagedType.LPWStr)] string lpKeyName, [In] [MarshalAs(UnmanagedType.LPWStr)] string lpString, [In] [MarshalAs(UnmanagedType.LPWStr)] string lpFileName);
 
-        [DllImport("kernel32", CharSet = CharSet.Unicode)]
-        public static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+        [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileStringW")]
+        public static extern uint GetPrivateProfileString([In] [MarshalAs(UnmanagedType.LPWStr)] string lpAppName, [In] [MarshalAs(UnmanagedType.LPWStr)] string lpKeyName, [In] [MarshalAs(UnmanagedType.LPWStr)] string lpDefault, [Out] [MarshalAs(UnmanagedType.LPWStr)] StringBuilder lpReturnedString, uint nSize, [In] [MarshalAs(UnmanagedType.LPWStr)] string lpFileName);
     }
 }
