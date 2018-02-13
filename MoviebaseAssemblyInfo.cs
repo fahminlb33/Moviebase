6f1f5d313494f99bd67ae071f7f7cd496b2bf26e
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2018-02-13 09:55:06 +0700

    using common assembly info

---
+++ MoviebaseAssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Moviebase")]
+[assembly: AssemblyDescription("Movie management tools")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Fahmi Noor Fiqri")]
+[assembly: AssemblyProduct("Moviebase")]
+[assembly: AssemblyCopyright("Copyright © Fahmi Noor Fiqri 2017")]
+[assembly: AssemblyTrademark("Moviebase")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("2.3.*")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
