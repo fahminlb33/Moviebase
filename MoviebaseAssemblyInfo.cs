7384e004ca7eea26c158f3cac4e0264cd31dd867
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2018-03-10 14:53:20 +0700

    update version

--- MoviebaseAssemblyInfo.cs
+++ MoviebaseAssemblyInfo.cs
@@ -9,10 +9,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fahmi Noor Fiqri")]
 [assembly: AssemblyProduct("Moviebase")]
-[assembly: AssemblyCopyright("Copyright © Fahmi Noor Fiqri 2017")]
+[assembly: AssemblyCopyright("Copyright © Fahmi Noor Fiqri 2018")]
 [assembly: AssemblyTrademark("Moviebase")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.3.*")]
+[assembly: AssemblyVersion("2.4.*")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
