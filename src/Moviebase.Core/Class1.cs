8aa330a10c6546d3906a0e30505b912191ff9cc2
fahminlb33 <fahminlb33@gmail.com>
2020-09-05 08:32:17 +0700

    Rename Core to Services to avoid conflict with WPF app

--- src/Moviebase.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Moviebase.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Moviebase.Services/Providers/SubtitleService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moviebase.Services.Providers
+{
+    class SubtitleService
+    {
+    }
+}
